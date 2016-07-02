f3568abeea11b45f4510953f5f4231376d380d70
ldq@zizizizizi.com <ldq@zizizizizi.com>
2016-07-02 21:54:38 +0800

    接口。

---
+++ app/src/main/java/com/fans/becomebeaut/home/ui/BeautyActivity.java
@@ -0,0 +1,26 @@
+package com.fans.becomebeaut.home.ui;
+
+import com.fans.becomebeaut.common.ui.AppBarActivity;
+
+/**
+ * 我要美容or美发
+ * Created by lu on 2016/7/2.
+ */
+public class BeautyActivity extends AppBarActivity{
+
+
+    @Override
+    protected int getContentViewId() {
+        return 0;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
